599ee18540cfe61a1a93667496ef4ca4c86b50e0
Sakamichi46-idol <pnlg11vs18ge@gmail.com>
2026-07-08 21:39:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 
 from media_getter import get_media
-
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
