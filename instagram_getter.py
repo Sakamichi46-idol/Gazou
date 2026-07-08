31134801a1e6874ba5fbf2da14e950016ad82b05
Sakamichi46-idol <pnlg11vs18ge@gmail.com>
2026-07-08 22:27:34 +0900

    Update instagram_getter.py

--- instagram_getter.py
+++ instagram_getter.py
@@ -1,4 +1,25 @@
+import yt_dlp
+
+
 def get_instagram(url):
-    print(f"Instagram URL: {url}")
+
+    ydl_opts = {
+        "quiet": True,
+        "skip_download": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+        info = ydl.extract_info(
+            url,
+            download=False
+        )
+
+    print("====== Instagram情報 ======")
+
+    for key in info.keys():
+        print(key)
+
+    print("===========================")
 
     return []
