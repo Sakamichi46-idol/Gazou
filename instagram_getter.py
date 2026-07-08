1745f32b0d86010eb13107ab89ef4947d421d082
Sakamichi46-idol <pnlg11vs18ge@gmail.com>
2026-07-08 22:44:21 +0900

    Update instagram_getter.py

--- instagram_getter.py
+++ instagram_getter.py
@@ -1,10 +1,28 @@
-import yt_dlp
-import tempfile
+import glob
 import os
+import tempfile
+
+import yt_dlp
 
 
 def get_instagram(url):
 
     temp_dir = tempfile.mkdtemp()
 
-    return temp_dir
+    ydl_opts = {
+        "outtmpl": os.path.join(temp_dir, "%(id)s.%(ext)s"),
+        "quiet": True,
+        "no_warnings": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    files = glob.glob(os.path.join(temp_dir, "*"))
+
+    print("ダウンロードしたファイル")
+
+    for file in files:
+        print(file)
+
+    return files
