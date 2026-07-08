409a8214c90ff22f8d4a757ca136189a336d2194
Sakamichi46-idol <pnlg11vs18ge@gmail.com>
2026-07-08 22:43:15 +0900

    Update instagram_getter.py

--- instagram_getter.py
+++ instagram_getter.py
@@ -1,22 +1,10 @@
-import instaloader
+import yt_dlp
+import tempfile
+import os
 
 
 def get_instagram(url):
 
-    print("Instagramアクセス開始")
+    temp_dir = tempfile.mkdtemp()
 
-    L = instaloader.Instaloader()
-
-    shortcode = url.split("/p/")[1].split("/")[0]
-
-    print("Shortcode:", shortcode)
-
-    post = instaloader.Post.from_shortcode(
-        L.context,
-        shortcode
-    )
-
-    print("取得成功")
-    print(post)
-
-    return []
+    return temp_dir
