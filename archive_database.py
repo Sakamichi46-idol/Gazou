04b7fcaf24fddf9a6ff410e305827d96d6283ac6
Sakamichi46-idol <pnlg11vs18ge@gmail.com>
2026-07-12 05:07:00 +0900

    Update archive_database.py

--- archive_database.py
+++ archive_database.py
@@ -2,17 +2,22 @@ import os
 import sqlite3
 
 
+
 # =========================
 # DB設定
 # =========================
 
 DB_DIR = "data"
+
 DB_NAME = "archive.db"
+
+
 
 os.makedirs(
     DB_DIR,
     exist_ok=True
 )
+
 
 
 DB_PATH = os.path.join(
@@ -21,42 +26,58 @@ DB_PATH = os.path.join(
 )
 
 
+
 print(
     f"ARCHIVE DB PATH: {DB_PATH}"
 )
 
 
 
+
+
 # =========================
-# 初期化
+# DB初期化
 # =========================
 
 def init_archive_db():
+
 
     conn = sqlite3.connect(
         DB_PATH
     )
 
+
     cur = conn.cursor()
+
 
 
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS archive
         (
+
             url TEXT PRIMARY KEY,
+
             group_name TEXT,
+
             member TEXT,
+
             title TEXT,
+
             date TEXT
+
         )
         """
     )
 
 
+
     conn.commit()
 
     conn.close()
+
+
+
 
 
 
@@ -66,11 +87,14 @@ def init_archive_db():
 
 def is_archived(url):
 
+
     conn = sqlite3.connect(
         DB_PATH
     )
 
+
     cur = conn.cursor()
+
 
 
     cur.execute(
@@ -85,13 +109,18 @@ def is_archived(url):
     )
 
 
+
     result = cur.fetchone()
+
 
 
     conn.close()
 
 
+
     return result is not None
+
+
 
 
 
@@ -100,46 +129,75 @@ def is_archived(url):
 # =========================
 
 def save_archive(
+
     group_name,
+
     member,
+
     title,
+
     date,
+
     url
+
 ):
+
 
     conn = sqlite3.connect(
         DB_PATH
     )
 
+
     cur = conn.cursor()
+
 
 
     cur.execute(
         """
         INSERT OR IGNORE INTO archive
         (
+
             url,
+
             group_name,
+
             member,
+
             title,
+
             date
+
         )
+
         VALUES
+
         (?, ?, ?, ?, ?)
+
         """,
+
         (
+
             url,
+
             group_name,
+
             member,
+
             title,
+
             date
+
         )
+
     )
+
 
 
     conn.commit()
 
     conn.close()
+
+
 
 
 
@@ -149,11 +207,14 @@ def save_archive(
 
 def archive_count():
 
+
     conn = sqlite3.connect(
         DB_PATH
     )
 
+
     cur = conn.cursor()
+
 
 
     cur.execute(
@@ -164,10 +225,13 @@ def archive_count():
     )
 
 
+
     count = cur.fetchone()[0]
 
 
+
     conn.close()
+
 
 
     return count
